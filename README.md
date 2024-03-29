70015ef9524c3ad8310afcfc0426509045e3fa49
Arnaud Pion <apion@paloaltonetworks.com>
2024-03-29 10:42:44 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # prisma-cloud-policy-as-code
+
+Prerequisites - create a terraform.tfvars file with the following variables :
+pcc_url = "api.eu.prismacloud.io"
+pcc_username = "<Insert Your Access Key>"
+pcc_userpass = "<Insert Your Access Secret>"
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "pcc_url" {
+  description = "Prisma Cloud Compute URL, use API format"
+  default = "api.eu.prismacloud.io"
+}
+
+variable "pcc_username" {
+  description = "Prisma Cloud Access Key"
+  sensitive   = true
+  
+}
+
+variable "pcc_userpass" {
+  description = "Prisma Cloud Secret"
+  sensitive   = true
+  
+}
